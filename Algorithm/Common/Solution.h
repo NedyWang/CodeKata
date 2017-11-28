d15e30f07eb29311fe4f0ecded067ca399cbac63
wgshub <wgsmail@163.com>
2017-11-28 18:22:14 +0800

    Add solution.h

---
+++ Algorithm/Common/Solution.h
@@ -0,0 +1,13 @@
+//
+// Created by root on 11/17/17.
+//
+
+#ifndef LEET_CODE_SOLUTION_H
+#define LEET_CODE_SOLUTION_H
+
+class Solution {
+public:
+    virtual void unitTest() = 0;
+};
+
+#endif //LEET_CODE_SOLUTION_H
