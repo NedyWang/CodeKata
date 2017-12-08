8c2185887f4540a429162ffe6af2eace1a0a0efd
wgshub <wgsmail@163.com>
2017-12-08 17:41:29 +0800

    Add poll tcp server

--- Alpha-Net/tcp_poll_server.cpp
+++ Alpha-Net/tcp_poll_server.cpp
@@ -6,9 +6,12 @@
 #include <cstring>
 #include <poll.h>
 #include <cstdio>
+#include <iostream>
+#include <zconf.h>
 
 #define PORT 8787
 #define MAX_LEN 1024
+#define MAX_CONNECTIONS 1000
 
 void tcp_poll_server()
 {
@@ -24,20 +27,57 @@ void tcp_poll_server()
     bind(listen_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
     listen(listen_fd, 5);
 
-    struct pollfd poll_set[FOPEN_MAX];
+    struct pollfd poll_set[MAX_CONNECTIONS];
+    for(int i = 1; i < MAX_CONNECTIONS; ++i) {
+        poll_set[i].fd = -1;
+    }
+
     poll_set[0].fd = listen_fd;
     poll_set[0].events = POLLRDNORM;
 
     int numbers = 1;
+    char buf[MAX_LEN];
 
     while (true) {
         int n_ready = poll(poll_set, numbers, -1);
 
         if (n_ready > 0) {
             if (poll_set[0].revents == POLLIN) {
-
+                --n_ready;
+                socklen_t client_addr_len = sizeof(client_addr);
+                int client_fd = accept(poll_set[0].fd, (struct sockaddr*)&client_addr, &client_addr_len);
+                bool is_full = true;
+                for(int i = 1; i < MAX_CONNECTIONS; ++i) {
+                    if (poll_set[i].fd == -1) {
+                        continue;
+                    }
+                    is_full = false;
+                    poll_set[i].fd = client_fd;
+                }
+                if (is_full) {
+                    std::cout<< "too many client connections." << std::endl;
+                    close(client_fd);
+                }
             }
-
+            int i = 1;
+            while(n_ready > 0 and i < MAX_CONNECTIONS) {
+                if (poll_set[i].revents == POLLIN) {
+                    int n = read(poll_set[i].fd, buf, MAX_LEN);
+                    if (n == 0) {
+                        // the connection is closed by client
+                        shutdown(poll_set[i].fd, SHUT_RDWR);
+                        poll_set[i].fd = -1;
+                    } else if (n < 0) {
+                        std::cout << "RST reveived" << std::endl;
+                        shutdown(poll_set[i].fd, SHUT_RD);
+                        poll_set[i].fd = -1;
+                    } else {
+                        write(poll_set[i].fd, buf, sizeof(buf));
+                    }
+                    --n_ready;
+                }
+                ++i;
+            }
         }
 
     }
