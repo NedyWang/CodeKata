a078fabc10d6687748aef6f6d81950801e7de388
wgshub <wgsmail@163.com>
2017-12-17 12:58:42 +0800

    Rename function

--- LeetCode/GenerateParentheses.cpp
+++ LeetCode/GenerateParentheses.cpp
@@ -2,9 +2,9 @@
 // Created by root on 11/29/17.
 //
 
-/*
- * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
- *
+/**
+ * Given n pairs of parentheses, write a function to generate
+ * all combinations of well-formed parentheses.
  * For example, given n = 3, a solution set is:
  * [
  *   "((()))",
@@ -13,29 +13,30 @@
  *   "()(())",
  *   "()()()"
  * ]
- *
  */
 
 #include <string>
 #include <vector>
 
-void do_generateParenthesis(std::vector<std::string> &result, std::string str, int n, int m) {
+void doGenerateParenthesis(std::vector<std::string> &result, std::string str, int n, int m)
+{
     if (n == 0 && m == 0) {
         result.push_back(str);
         return;
     }
 
     if (n > 0) {
-        do_generateParenthesis(result, str + "(", n - 1, m + 1);
+        doGenerateParenthesis(result, str + "(", n - 1, m + 1);
     }
 
     if (m > 0) {
-        do_generateParenthesis(result, str + ")", n, m - 1);
+        doGenerateParenthesis(result, str + ")", n, m - 1);
     }
 }
 
-std::vector<std::string> generateParenthesis(int n) {
+std::vector<std::string> generateParenthesis(int n)
+{
     std::vector<std::string> result;
-    do_generateParenthesis(result, "", n, 0);
+    doGenerateParenthesis(result, "", n, 0);
     return result;
 }
