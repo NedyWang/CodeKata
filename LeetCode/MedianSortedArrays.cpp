d4cc228b987f5dbd93b48250c2eabfab80593f9a
wgshub <wgsmail@163.com>
2017-12-17 15:00:00 +0800

    Rewrite getKth

--- LeetCode/MedianSortedArrays.cpp
+++ LeetCode/MedianSortedArrays.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 
-/*
+/**
 *There are two sorted arrays nums1 and nums2 of size m and n respectively.
 * Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
 *
@@ -58,4 +58,33 @@ double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
         return get_kth(nums1, nums2, totalNum / 2 + 1);
     else
         return double(get_kth(nums1, nums2, totalNum / 2) + get_kth(nums1, nums2, totalNum / 2 + 1)) / 2;
+}
+
+int getKth(int *begin_A, int *end_A, int *begin_B, int *end_B, unsigned long k)
+{
+    auto len_A = (end_A - begin_A)/sizeof(int);
+    auto len_B = (end_B - begin_B)/sizeof(int);
+
+    if (len_A > len_B) {
+        return getKth(begin_B, end_B, begin_A, end_A, k);
+    }
+
+    if (len_A == 0) {
+        return *(begin_B + k);
+    }
+
+    if (k == 1) {
+        return std::min(*begin_A, *begin_B);
+    }
+
+    auto nth_A = std::min(len_A, k >> 1);
+    auto nth_B = k - nth_A;
+
+    if (*(begin_A + nth_A - 1) == *(begin_B + nth_B - 1)) {
+        return *(begin_A + nth_A - 1);
+    } else if (*(begin_A + nth_A - 1) < *(begin_B + nth_B - 1)) {
+        return getKth(begin_A + nth_A, end_A, begin_B, end_B, nth_B);
+    } else {
+        return getKth(begin_A, end_A, begin_B + nth_B, end_B, nth_A);
+    }
 }
